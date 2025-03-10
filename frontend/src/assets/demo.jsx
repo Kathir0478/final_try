2da3842eeb91d673799be1eb4e0fe5a3da311e04
kathiravan <kathir200420@gmail.com>
2025-03-10 10:55:18 +0530

    2

--- frontend/src/assets/demo.jsx
+++ frontend/src/assets/demo.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const base = "http://localhost:5000/api"
-=======
-const base = "https://final-try-backend.onrender.com/api"
->>>>>>> 18f3be6ab9fa0e717b7bf413146daca66d6cdc9e
 export const demo = {
     "login": `${base}/login`,
     "signup": `${base}/signup`,
